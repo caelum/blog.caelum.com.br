69063daec8724e00e70d9b6a06bbc66afb0b549c
Gabriel Oliveira <gabriel.so@gmail.com>
2011-02-16 14:50:55 -0200

    Melhorando o widget de Twitter

--- includes/js/tfcWidget.js
+++ includes/js/tfcWidget.js
@@ -14,9 +14,10 @@
             avatar_elm = $("<img>", { id : "tfc-avatar", src : data.profile_image_url }),
             name_elm = $("<h5>", { id : "tfc-name" }).text(data.name),
             screen_name_elm = $("<p>", { id : "tfc-screen_name" }).text("@" + data.screen_name),
-            followers_count_elm = $("<h4>", { id : "tfc-followers_count" }).text(data.followers_count + " seguidores");
+            followers_count_elm = $("<h4>", { id : "tfc-followers_count" }).text(data.followers_count + " seguidores"),
+            current_status_elm = $("<q>", { id : "tfc-status", cite : "http://www.twitter.com/" + user + "/statuses/" + data.status.id_str }).text(data.status.text);
 
-        context.append(profile_link_elm.append(name_elm.prepend(avatar_elm)).append(screen_name_elm).append(followers_count_elm));
+        context.append(profile_link_elm.append(name_elm.prepend(avatar_elm)).append(screen_name_elm).append(followers_count_elm).append(current_status_elm));
       }
     );
   }
